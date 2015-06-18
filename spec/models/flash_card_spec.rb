fdd8dd01dd973e3d04d4586a8fdad197a2bbc3f1
William Noll <williampnoll@gmail.com>
2015-06-17 21:43:21 -0400

    Add Initial Tests for Flash Cards

--- spec/models/flash_card_spec.rb
+++ spec/models/flash_card_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe FlashCard, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'without a stimulus' do
+    subject(:flash_card) { FlashCard.new(response: "June 22", set_id: 1) }
+    it { should_not be_valid }
+  end
+
+  context 'without a response' do
+    subject(:flash_card) { FlashCard.new(stimulus: "When does summer officially begin?", set_id: 1) }
+    it { should_not be_valid }
+  end
+
+  context 'with a stimulus, response, and set id' do
+    subject(:flash_card) { FlashCard.new(stimulus: "When does summer officially begin?", response: "June 22", set_id: 1) }
+
+    it { should be_valid }
+    it { should respond_to(:stimulus) }
+    it { should respond_to(:response) }
+    it { should respond_to(:set_id) }
+  end
 end
