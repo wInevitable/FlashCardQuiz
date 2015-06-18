49be9f73acf29394211899b761431b154bf14fd8
William Noll <williampnoll@gmail.com>
2015-06-17 21:49:57 -0400

    Add Additional Tests and Validations to FlashCard Model

--- app/models/flash_card.rb
+++ app/models/flash_card.rb
@@ -1,2 +1,3 @@
 class FlashCard < ActiveRecord::Base
+  validates :stimulus, :response, :set_id, presence: :true
 end
--- spec/models/flash_card_spec.rb
+++ spec/models/flash_card_spec.rb
@@ -1,18 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe FlashCard, type: :model do
+  let(:stimulus) { "When does summer officially begin?" }
+  let(:response) { "June 22" }
+
   context 'without a stimulus' do
-    subject(:flash_card) { FlashCard.new(response: "June 22", set_id: 1) }
+    subject(:flash_card) { FlashCard.new(response: :response, set_id: 1) }
     it { should_not be_valid }
   end
 
   context 'without a response' do
-    subject(:flash_card) { FlashCard.new(stimulus: "When does summer officially begin?", set_id: 1) }
+    subject(:flash_card) { FlashCard.new(stimulus: :stimulus, set_id: 1) }
+    it { should_not be_valid }
+  end
+
+  context 'without a set_id' do
+    subject(:flash_card) { FlashCard.new(stimulus: :stimulus, response: :response) }
     it { should_not be_valid }
   end
 
   context 'with a stimulus, response, and set id' do
-    subject(:flash_card) { FlashCard.new(stimulus: "When does summer officially begin?", response: "June 22", set_id: 1) }
+    subject(:flash_card) { FlashCard.new(stimulus: :stimulus, response: :response, set_id: 1) }
 
     it { should be_valid }
     it { should respond_to(:stimulus) }
